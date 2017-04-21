6b21764d46a324c8d77fe201c35e13255db49519
melisgul <gulenay@itu.edu.tr>
2017-04-21 14:46:15 +0300

    .

--- skybotapp/views.py
+++ skybotapp/views.py
@@ -5,8 +5,8 @@ from django.http.response import HttpResponse
 # Create your views here.
 class SkyBotView(generic.View):
     def get(self, request, *args, **kwargs):
-        if self.request.GET.get['hub.verify_token'] == '20170421':
-            return HttpResponse(self.request.GET.get['hub.challenge'])
+        if self.request.POST.get['hub.verify_token'] == '20170421':
+            return HttpResponse(self.request.POST.get['hub.challenge'])
         else:
             return HttpResponse('Error, invalid token')
       
