d3d1adfdc358600397f708633a7658f72c32c9d9
Melis Gülenay <gulenay@itu.edu.tr>
2017-04-21 13:37:53 +0300

    .

--- skybotapp/views.py
+++ skybotapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+# yomamabot/fb_yomamabot/views.py
+from django.views import generic
+from django.http.response import HttpResponse
 # Create your views here.
+class SkyBotView(generic.View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Hello World!")
