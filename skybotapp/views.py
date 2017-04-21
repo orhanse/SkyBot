4deabb35e01011d367769cd3d78da4df31aa42fc
melisgul <gulenay@itu.edu.tr>
2017-04-21 15:02:28 +0300

    message

--- skybotapp/views.py
+++ skybotapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 # yomamabot/fb_yomamabot/views.py
 from django.views import generic
 from django.http.response import HttpResponse
@@ -9,8 +10,24 @@ class SkyBotView(generic.View):
             return HttpResponse(self.request.GET['hub.challenge'])
         else:
             return HttpResponse('Error, invalid token')
-      
 
-#@method_decorator(csrf_exempt)
-#def dispatch(self, request, *args, **kwargs):
-#    return generic.View.dispatch(self, request, *args, **kwargs)
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return generic.View.dispatch(self, request, *args, **kwargs)
+          
+    def post(self, request, *args, **kwargs):
+        # Converts the text payload into a python dictionary
+        incoming_message = json.loads(self.request.body.decode('utf-8'))
+        # Facebook recommends going through every entry since they might send
+        # multiple messages in a single call during high load
+        for entry in incoming_message['entry']:
+            for message in entry['messaging']:
+                # Check to make sure the received call is a message call
+                # This might be delivery, optin, postback for other events 
+                if 'message' in message:
+                    # Print the message to the terminal
+                    pprint(message)     
+        return HttpResponse()
+
+
+
