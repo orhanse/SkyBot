8db7ffc19822de18ca243a72a37ece705ff3ccb0
egnkya <eginkaya@itu.edu.tr>
2017-04-23 13:29:14 +0300

    .

--- skybotapp/views.py
+++ skybotapp/views.py
@@ -31,7 +31,7 @@ def witConnect(incoming_message):
     }  
     client = Wit(access_token='KVCNXSS7SD5RENA5PQ6QBS242ETDIBHC', actions=actions)
     try:
-        resp = client.run_actions(incoming_message)
+        resp = client.runActions(incoming_message)
         pprint('Yay, got Wit.ai response: ' + str(resp))
         return resp
     except:
