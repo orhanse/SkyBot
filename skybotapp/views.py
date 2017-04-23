93b02f87acc05f2decf4f9954a90566fc9064f1b
egnkya <eginkaya@itu.edu.tr>
2017-04-23 17:39:21 +0300

    bugfix

--- skybotapp/views.py
+++ skybotapp/views.py
@@ -83,20 +83,20 @@ class SkyBotView(generic.View):
 def parseWitData(witOut):
     scannerInput = []
     if witOut['entities']['location'][0]['value']!= None:
-        scannerInput[0] = witOut['location'][0]['value']
+        scannerInput[0] = witOut['entities']['location'][0]['value']
     else:
         return 'I couldnt find any location info in your message. Please enter your flight "from x to y"'
     
     if witOut['entities']['location'][1]['value']!= None:
-        scannerInput[1] = witOut['location'][1]['value']
+        scannerInput[1] = witOut['entities']['location'][1]['value']
     else:
         return 'I couldnt find your destination location info in your message. Please enter your flight "from x to y"'
     
     if witOut['entities']['datetime'][0]['value']!= None:
-        scannerInput[2] = witOut['datetime'][0]['value']
+        scannerInput[2] = witOut['entities']['datetime'][0]['value']
         
     if witOut['entities']['datetime'][1]['value']!= None:
-        scannerInput[2] = witOut['datetime'][1]['value']   
+        scannerInput[2] = witOut['entities']['datetime'][1]['value']   
     return scannerInput
 
 def homeView(request):
