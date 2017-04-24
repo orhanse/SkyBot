02c70e2298f6dbfed7b33e1e2669c0f8099d3515
melisgul <gulenay@itu.edu.tr>
2017-04-25 02:24:32 +0300

    migrations

--- skybotapp/models.py
+++ skybotapp/models.py
@@ -5,6 +5,6 @@ class botDB(models.Model):
     userId = models.CharField(max_length = 100)
     firstLocation = models.CharField(max_length = 100)
     secondLocation = models.CharField(max_length = 100)
-    firstDate = models.DateField(max_length = 100)
-    secondDate = models.DateField(max_length = 100)
+    firstDate = models.CharField(max_length = 100)
+    secondDate = models.CharField(max_length = 100)
   
