71c4f8eabeec65a275ff23032c830bcbd860a1d7
Melis Gülenay <gulenay@itu.edu.tr>
2017-04-24 14:37:40 -0700

    botDB added to the project

--- skybotapp/models.py
+++ skybotapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class botDB(models.Model):
+    userId = models.CharField()
+    firstLocation = models.CharField()
+    secondLocation = models.CharField()
+    firstDate = models.DateField()
+    secondDate = models.DateField()
+  
