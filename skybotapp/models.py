82f618ca564b040f2a4f187e7298e6196ff3d2b9
melisgul <gulenay@itu.edu.tr>
2017-04-25 01:03:14 +0300

    db creation

--- skybotapp/models.py
+++ skybotapp/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class botDB(models.Model):
-    userId = models.CharField()
-    firstLocation = models.CharField()
-    secondLocation = models.CharField()
-    firstDate = models.DateField()
-    secondDate = models.DateField()
+    userId = models.CharField(max_length = 100)
+    firstLocation = models.CharField(max_length = 100)
+    secondLocation = models.CharField(max_length = 100)
+    firstDate = models.DateField(max_length = 100)
+    secondDate = models.DateField(max_length = 100)
   
