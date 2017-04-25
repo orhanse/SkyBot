a596a290a60eed22353bd6aa26a63df172144644
Melis Gülenay <gulenay@itu.edu.tr>
2017-04-25 01:41:35 -0700

    bugfix

--- SkyBot/settings.py
+++ SkyBot/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
