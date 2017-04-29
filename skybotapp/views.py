9e17957742ea06390ea0e2c8a07d81d6fa679c21
egnkya <eginkaya@itu.edu.tr>
2017-04-29 09:17:18 +0300

    .

--- skybotapp/views.py
+++ skybotapp/views.py
@@ -182,9 +182,9 @@ def checkArray(array):
 def homeView(request):
     return HttpResponse('Hello')
 
-def flight(list):
+def flight(query):
     pprint('CHEAPEST')
-    #pprint('query = ' + str(query))
+    pprint('query = ' + str(query))
     
     result = json.loads(json.dumps({'price': 0, 'out': {'date': '', 'from': '', 'to': '', 'carrier': ''}, 'in': {'date': '', 'from':'', 'to': '', 'carrier':''}}))
     
